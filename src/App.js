d4eba611498a3dadc76d26dcf95a8474ae138667
antbravo121 <antbravo121@gmail.com>
2022-12-16 08:32:14 -0500

    content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PhotoMe from "./images/me.png"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello my name is Anthony</h1>
+      <h1>Hi, I'm Anthony!</h1>
+      <img src={PhotoMe} />
+      <h2>I bench 225</h2>
     </div>
   );
 }
